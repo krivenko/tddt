64e1f7e9234981bde1a395243e3742d9fd70348b
Viktor Harkov <viktor.harkov@hotmail.com>
2024-01-24 10:23:18 +0000

    created a new folder where to put my scripts. DTRILEX is the file where I write the time-dependent D-TRIELX code.

--- doc/scripts_viktor/DTRILEX.py
+++ doc/scripts_viktor/DTRILEX.py
@@ -39,9 +39,11 @@ from tddt.vie2 import solve_vie2
 
 # Model parameters
 U = 3.0
-mu = 0.5 # nearest neighbor hopping 
+mu = 0.5 * U
+#eps = 0.2
+t1 = 1.0 # nearest neighbor hopping 
 t2 = 0.0 # next nearest neighbor hopping
-A = 0.10 
+A = 0.1
 
 
 # time-mesh
@@ -87,8 +89,9 @@ def V(axis,sign,t):
 
 # Lattice despersion
 def eps_k(t1,t2,k,time):
-    eps_k = -2.0*t1*(np.cos(k[0]-A*np.cos(time))+np.cos(k1]-A*np.cos(time))))\
-    return eps_k*t2*np.cos(k[0]-A*np.cos(time))*np.cos(k[
+    eps_k = -2.0*t1*(np.cos(k[0]-A*np.cos(time))+np.cos(k[1]-A*np.cos(time)))\
+            -4.0*t2*np.cos(k[0]-A*np.cos(time))*np.cos(k[1]-A*np.cos(time))
+    return eps_k
 
 
 # Make Keldysh GF of a single fermion with energy `eps` and occupation number `occup_n`
@@ -122,7 +125,8 @@ h0 = -mu * (n('up', 0) + n('dn', 0)) + U * n('up', 0) * n('dn', 0) \
 #h0 += sum(V('y', 1) * c_dag(sn, 0) * c(sn, 1) + V('y', -1) * c_dag(sn, 1) * c(sn, 0)
 #          for sn in spin_names)
 #h0 += sum(V('x', 1) * c_dag(sn, 0) * c(sn, 2) + V('x', -1) * c_dag(sn, 2) * c(sn, 0)
-#          for sn in spin_dag(sn, 0) * c(sn, 3) + V('y', 1) * c_dag(sn, 3) * c(sn, 0)
+#          for sn in spin_names)
+#h0 += sum(V('y', -1) * c_dag(sn, 0) * c(sn, 3) + V('y', 1) * c_dag(sn, 3) * c(sn, 0)
 #          for sn in spin_names)
 #h0 += sum(V('x', -1) * c_dag(sn, 0) * c(sn, 4) + V('x', 1) * c_dag(sn, 4) * c(sn, 0)
 #          for sn in spin_names)
@@ -159,8 +163,10 @@ h = h0 + \
 
 params = {}
 params['verbosity'] = 2                      # Verbosity level
+params['hbar'] = 1.0                         # Planck's constant
 params['hamiltonian_interpol'] = 'Trapezoid' # Trapezoid rule interpolation of H(t)
 params['lanczos_min_matrix_size'] = 40       # Use LAPACK for subspaces of dim 32 and smaller
+
 
 # Impurity GF
 gf_struct = [('up', 1), ('dn', 1)]
@@ -310,4 +316,284 @@ for br1 in branches:
 print('Q is Hermitian:',is_hermitian(Q))
 
 exit()
+#eps_k = from_lesser_greater(eps_k_l, eps_k_l) #!
 
+print(eps_k_test[FW,FW].data.shape)
+
+print('esp_k_spin:',eps_k_test[FW,BW].data[0,0,0,:,:])
+print('esp_k_spin:',eps_k_test[FW,BW].data[1,1,0,:,:])
+print('esp_k_spin:',eps_k_test[FW,BW].data[0,0,1,:,:])
+print('esp_k_spin:',eps_k_test[FW,BW].data[1,1,1,:,:])
+
+print('is Hermitian:',is_hermitian(eps_k_test))
+exit()
+print(eps_k==eps_k_test)
+#print('eps_k: ', eps_k[FW,FW].data.shape)
+print('delta: ', delta[FW,FW].data.shape)
+#print(eps_k.non_time_mesh.components)
+#print(dir(eps_k.non_time_mesh))
+
+for br1 in branches:
+    for br2 in branches:
+        eps_tild_k[br1,br2].data[...] = eps_k[br1,br2].data - delta[br1,br2].data[:,:,None,:,:]
+
+for br1 in branches:
+    for br2 in branches:
+        for k in (bz_mesh):
+            gf_imp_on_k_mesh[br1,br2].data[:,:,k.linear_index,:,:] = gf_imp[br1,br2].data[:,:,:,:]
+
+
+#eps_tildgf_imp_conv = eps_tild_k @ gf_imp
+F = eps_tild_k @ gf_imp
+
+#for sp1 in (0,1):
+#    for sp2 in (0,1):
+#        for sp3 in (0,1):
+#            print(sp1)
+#            for br1 in branches:
+#                for br2 in branches:
+#                    eps_tild_gf_imp_vareps[br1,br2].data[:,:,:,sp1,sp2] \
+#                     = eps_tildgf_imp_conv[br1,br2].data[:,:,:,sp1,sp3] \
+#                                   * eps_k[br1,br2].data[:,:,:,sp3,sp2]
+
+
+for sp in (0,1):
+    print(sp)
+    for br1 in branches:
+        for br2 in branches:
+            eps_tild_gf_imp_vareps[br1,br2].data[...] \
+                        = F[br1,br2].data[:,:,:,:,None,sp] \
+                          * eps_k[br1,br2].data[:,:,:,sp,None,:]
+
+Q = KeldyshGF(mesh=ttk_mesh, arg_index_shapes=((2,), (2,)))
+
+for br1 in branches:
+    for br2 in branches:
+        Q[br1,br2].data[...] = -delta[br1,br2].data[:,:,None,:,:] + eps_tild_gf_imp_vareps[br1,br2].data
+
+
+#G = solve_vie2(F, Q)
+#G = solve_vie2(F, F)
+G = solve_vie2(F, eps_k_test)
+#G = solve_vie2(F,eps_tild_gf_imp_vareps)
+exit()
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+#for k in (bz_mesh):
+#    print(k.linear_index)
+#print(dir(bz_mesh))
+
+print(eps_tild_k.arg_index_shapes)
+print(gf_imp_on_k_mesh.arg_index_shapes)
+
+eps_tild_k @ gf_imp_on_k_mesh
+
+
+
+
+
+
+####
+exit()
+
+# Points on the real time axis 
+#t_points = list(t_mesh)
+time1, time2 = t_points[0], t_points[1]
+# Contour points
+z1 = ContourPoint(Branch.FORWARD, time1)
+z2 = ContourPoint(Branch.BACKWARD, time2)
+
+print(gf)
+print(dir(gf['dn']))
+print(*gf)
+print(gf['dn'].components.shape)
+print(gf['dn'].components)
+print(gf['dn'][FW,FW])
+print(gf['dn'][FW,FW].data)
+print('##########')
+print(gf['dn'][z1,z2])
+print(gf['dn'][z1,z2].data)
+print('FW: ', FW)
+print('BW: ', BW)
+print('time1: ', time1)
+print('time2: ', time2)
+print('z1: ', z1)
+print('z2: ', z2)
+
+#print(gf[z1,z2])
+#print(gf['dn'].shape)
+#g=gf['dn'].components.reshape(4)
+
+
+print(gf['up'].n_args)
+
+#exit()
+#print(type(gf))
+#print(type(gf['dn']))
+#print(type(tri_vertex_ch))
+
+print(dir(gf['dn']))
+print(gf['dn'].arg_index_shapes)
+#print(g.arg_index_shapes)
+#print(tri_vertex_ch.shape())
+#print(tri_vertex_ch.arg_index_shapes)
+
+#C = conv(gf['dn'], eps_k,
+#         [(0, 1), (1, 0)])
+#C = conv(gf['dn'], gf['dn'],
+#         [(0, 1)])
+print(gf['dn'].components.data.shape)
+
+# Vertex
+arg_index_shapes = ((2,), #((2, 3),    # \sigma_1, l_1
+                    (2,), #(2, 3),    # \sigma_2, l_2
+                    (2,)) #(4, 3, 3)) # \varsigma, l_3, l_4
+
+Lambda = KeldyshGF(mesh=MeshProduct(t_mesh, t_mesh, t_mesh), arg_index_shapes=arg_index_shapes)
+
+
+#density_f_0 = compute_expectval(n('up',0) + n('dn',0), init_state, h, t_mesh, params)
+
+#print('density: ' ,density_f_0)
+#print('n: ',n('up', 0) + n('dn', 0))
+#rho = (n('up', 0) + n('dn', 0)) - density_f_0
+
+
+#exit()
+tri_vertex_ch_uu = compute_keldysh_vertex3(('up', 0), # c_indices
+                                        ('up', 0), # c_dag_indices
+                                        n('dn', 0) + n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_ch_ud = compute_keldysh_vertex3(('up', 0), # c_indices
+                                        ('dn', 0), # c_dag_indices
+                                        n('dn', 0) + n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_ch_du = compute_keldysh_vertex3(('dn', 0), # c_indices
+                                        ('up', 0), # c_dag_indices
+                                        n('dn', 0) + n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_ch_dd = compute_keldysh_vertex3(('dn', 0), # c_indices
+                                        ('dn', 0), # c_dag_indices
+                                        n('dn', 0) + n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+
+tri_vertex_sp_uu = compute_keldysh_vertex3(('up', 0), # c_indices
+                                        ('up', 0), # c_dag_indices
+                                        n('dn', 0) - n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_sp_ud = compute_keldysh_vertex3(('up', 0), # c_indices
+                                        ('dn', 0), # c_dag_indices
+                                        n('dn', 0) - n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_sp_du = compute_keldysh_vertex3(('dn', 0), # c_indices
+                                        ('up', 0), # c_dag_indices
+                                        n('dn', 0) - n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_sp_dd = compute_keldysh_vertex3(('dn', 0), # c_indices
+                                        ('dn', 0), # c_dag_indices
+                                        n('dn', 0) - n('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+tri_vertex_test = compute_keldysh_vertex3(('dn', 0), # c_indices
+                                        ('dn', 0), # c_dag_indices
+                                        c_dag('dn', 0)*c_dag('up', 0), # n_op
+                                        init_state,
+                                        h,
+                                        t_mesh,
+                                        params)
+
+#tri_vertex_test2 = compute_keldysh_vertex3(('dn', 0), # c_indices
+#                                        ('dn', 0), # c_dag_indices
+#                                        rho, # n_op
+#                                        init_state,
+#                                        h,
+#                                        t_mesh,
+#                                        params)
+
+print(Lambda[FW,BW,BW].data.shape) #(3, 3, 3, 2, 2, 4)
+#print(Lambda[FW,BW,BW].data[0,0,0,0,0,0,0,0,0,0])
+print('Lambda: ', Lambda)
+print(tri_vertex_ch_uu)
+print(tri_vertex_ch_uu[FW,BW,BW].data.shape)
+print('tri_vertex_ch_uu: ', tri_vertex_ch_uu)
+
+
+for br1 in branches:
+    for br2 in branches:
+        for br3 in branches:
+            Lambda[br1,br2,br3].data[:,:,:,0,0,0] = tri_vertex_ch_uu[br1,br2,br3].data
+            Lambda[br1,br2,br3].data[:,:,:,0,1,0] = tri_vertex_ch_ud[br1,br2,br3].data
+            Lambda[br1,br2,br3].data[:,:,:,1,0,0] = tri_vertex_ch_du[br1,br2,br3].data
+            Lambda[br1,br2,br3].data[:,:,:,1,1,0] = tri_vertex_ch_dd[br1,br2,br3].data
+
+            Lambda[br1,br2,br3].data[:,:,:,0,0,1] = tri_vertex_sp_uu[br1,br2,br3].data
+            Lambda[br1,br2,br3].data[:,:,:,0,1,1] = tri_vertex_sp_ud[br1,br2,br3].data
+            Lambda[br1,br2,br3].data[:,:,:,1,0,1] = tri_vertex_sp_du[br1,br2,br3].data
+            Lambda[br1,br2,br3].data[:,:,:,1,1,1] = tri_vertex_sp_dd[br1,br2,br3].data
+
+
+eps_k_l = Gf(mesh=ttk_mesh, target_shape=(2, 2)) #TODO: change to KeldyshGF 
+#g_k_= KeldyshGF(mesh=ttk_mesh, arg_index_shapes=((2,), (2,)))
+eps_tild_gf_imp_vareps = KeldyshGF(mesh=ttk_mesh, arg_index_shapes=((2,), (2,)))
+
+print(eps_k_l.data[0,1,:,1,1])
+for k in (bz_mesh):
+    #print(k)
+    for time1, time2 in MeshProduct(t_mesh, t_mesh):
+        if time1.value == time2.value:
+            eps_k_l[time1, time2, k] = eps_k(time1,time2,k)
+
+#print(Q_l.data.shape)
+#print(Q_l.data[0,0,:,0,0])
+#print(Q_l.data[0,0,:,1,0])
+#print(Q_l.data[0,0,:,0,1])
+#print(Q_l.data[0,0,:,1,1])
+#exit()
+#print(Q_l.data[0,1,:,0,0])
+#print(Q_l.data[1,1,:,0,0])
+#print(Q_l)
+eps_k = from_lesser_greater(eps_k_l, eps_k_l)
+
+print('eps_k: ', eps_k[FW,FW].data.shape)
+print('delta: ', delta[FW,FW].data.shape)
+
+for br1 in branches:
+    for br2 in branches:
+        eps_tild_k[br1,br2].data[...] = eps_k[br1,br2].data - delta[br1,br2].data[:,:,None,:,:]
+
+eps_tildgf_imp_conv = eps_tild_k @ gf_imp
+
+exit()
